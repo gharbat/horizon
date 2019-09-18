9258ea250bdc1121f6ae3b6cfb1381314484fbcd
Mohammed Ghrabat <Ar0799298231>
2019-09-18 10:16:37 +0300

    fp

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,7 +38,7 @@
                                 </div>
                                 <p>
                                     m is simply dummy the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.                                </p>
-                                <a class="btn-style-1" href="#">More About</a>
+
                             </div>
                         </div>
                         <div class="col-md-6">
